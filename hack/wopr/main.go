ab2b9c66093f82ab19fe0e5984a7a6f2ef4cf89b
Ethan Blackwelder <eblackwelder@google.com>
2021-06-14 12:58:21 -0700

    Stub game server

--- hack/wopr/main.go
+++ hack/wopr/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+const (
+	PORT = 8080
+)
+
+type ticTacToeServer struct{}
+
+func (s ticTacToeServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//TODO: create a "game" instance
+	//TODO: handle "click" events
+	io.WriteString(w, "Tic-tac-toe!\n")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO: pick a game
+	io.WriteString(w, "Hello, world!\n")
+}
 
 func main() {
-	fmt.Println("woot")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	mux.Handle("/ttt", ticTacToeServer{})
+
+	fmt.Printf("Listening on :%d\n", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux))
 }
