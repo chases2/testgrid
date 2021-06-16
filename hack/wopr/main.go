839d1430c62d55241b556c42fea27d2b77adda66
Ethan Blackwelder <eblackwelder@google.com>
2021-06-16 15:46:31 -0700

    Style WOPR

--- hack/wopr/main.go
+++ hack/wopr/main.go
@@ -23,17 +23,47 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 <html>
 <head>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
 
+body {
+	background-color: #125;
+	color: lightblue;
+	font-family: 'VT323', monospace;
+	font-size: x-large;
+}
+a,
+a:link,
+a:visited {
+	color: lightblue;
+	text-decoration: none;
+}
+a:hover {
+	filter: brightness(85%);
+}
+a:active {
+	filter: brightness(125%);
+}
+ul {
+	list-style: none;
+	margin-left: 0;
+	font-size: 120%;
+}
+.center {
+	margin: auto;
+	width: 50%;
+	padding: 10px;
+}
 </style>
 </head>
 <body>
-SHALL WE PLAY A GAME?
-
-<a href="/toe/new">Tic-tac-toe</a>
-<a href="http://mine-dot-k8s-testgrid.appspot.com/minesweeper/Ov-BAwEBBmFjdGlvbgH_ggABBAEFQm9hcmQB_4gAAQRGbGFnAQIAAQNOZXcBAgABBE9wZW4B_4oAAAAU_4cCAQEFYm9hcmQB_4gAAf-GAAAN_4UCAQL_hgAB_4QAADH_gwMBAv-EAAEEAQRNaW5lAQIAAQRPcGVuAQIAAQRGbGFnAQIAAQVNYXliZQECAAAAH_-JAwEBBVBvaW50Af-KAAECAQFYAQQAAQFZAQQAAAAF_4IDAQA=">Minesweeper</a>
-&nbsp;
-<!-- TODO: easter egg! -->
-<!-- a href="/global-thermonuclear-war">Global Thermo-nuclear War</a -->
+<div class="center">
+	<h1>SHALL WE PLAY A GAME?</h1>
+	<ul>
+		<li><a href="/toe/new">Tic-tac-toe</a></li>
+		<li><a href="http://mine-dot-k8s-testgrid.appspot.com/minesweeper/Ov-BAwEBBmFjdGlvbgH_ggABBAEFQm9hcmQB_4gAAQRGbGFnAQIAAQNOZXcBAgABBE9wZW4B_4oAAAAU_4cCAQEFYm9hcmQB_4gAAf-GAAAN_4UCAQL_hgAB_4QAADH_gwMBAv-EAAEEAQRNaW5lAQIAAQRPcGVuAQIAAQRGbGFnAQIAAQVNYXliZQECAAAAH_-JAwEBBVBvaW50Af-KAAECAQFYAQQAAQFZAQQAAAAF_4IDAQA=">Minesweeper</a></li>
+		<li><a href="/global-thermonuclear-war">Global Thermo-nuclear War</a></li>
+	</ul>
+</div>
 </body>
 </html>
 `)
@@ -53,11 +83,54 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/toe/", http.StripPrefix("/toe", tictactoe.CreateMux(gcsWrite)))
-	mux.HandleFunc("/global-thermonuclear-war", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "the only winning move is not to play")
-	})
+	mux.HandleFunc("/global-thermonuclear-war", playGlobalThermonuclearWar)
 	mux.HandleFunc("/", indexHandler)
 
 	fmt.Printf("Listening on :%d\n", PORT)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux))
+}
+
+func playGlobalThermonuclearWar(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, `
+	<html>
+	<head>
+	<style>
+	@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+	body {
+		background-color: #125;
+		color: lightblue;
+		font-family: 'VT323', monospace;
+		font-size: x-large;
+	}
+	a,
+	a:link,
+	a:visited {
+		color: lightblue;
+		text-decoration: none;
+	}
+	a:hover {
+		filter: brightness(85%);
+	}
+	a:active {
+		filter: brightness(125%);
+	}
+	ul {
+		list-style: none;
+		margin-left: 0;
+		font-size: 120%;
+	}
+	.center {
+		margin: auto;
+		width: 50%;
+		padding: 10px;
+	}
+	</style>
+	</head>
+	<body>
+	<div class="center">
+		the only winning move is not to play
+	</div>
+	</body>
+	</html>	
+`)
 }
