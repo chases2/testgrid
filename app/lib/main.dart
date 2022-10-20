f1742835dbd60da9370f7029e5dd0df8c9fbd234
Sean Chase <slchase@google.com>
2022-10-20 11:04:37 -0700

    Create client connection
    Client connection works for localhost, but only on linux version (not
    web)

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:app/pb/api/v1/data.pbgrpc.dart';
 import 'package:flutter/material.dart';
+import 'package:grpc/grpc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,93 +17,91 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ConfigReader(title: 'Config Reader Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ConfigReader extends StatefulWidget {
+  ConfigReader({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ConfigReader> createState() => _ConfigReaderState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _input = "";
+class _ConfigReaderState extends State<ConfigReader> {
+  // TODO(slchase): add an input box to the widget for scope
+  final String _scope = "gs://slchase-testgrid";
+  final List<String> _error = [
+    'Error with API call; check console',
+    'Is the API running at localhost:50051?'
+  ];
 
-  void _incrementCounter() {
+  List<String> _list = ['Press Button to call API'];
+
+  final stub = TestGridDataClient(ClientChannel('localhost',
+      port: 50051,
+      options:
+          const ChannelOptions(credentials: ChannelCredentials.insecure())));
+
+  void getAllDashboardNames() async {
+    ListDashboardResponse resp;
+    try {
+      resp = await stub.listDashboard(ListDashboardRequest(
+        scope: _scope,
+      ));
+    } catch (e) {
+      setState(() {
+        _list = _error;
+      });
+      rethrow;
+    }
+
+    List<String> dashNames = [];
+
+    resp.dashboards.forEach((dashboard) => dashNames.add(dashboard.name));
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _list = dashNames;
     });
   }
 
+  // nb: call setState(func) instead of calling build when you need to refresh
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: getTextListWidget(_list)),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: getAllDashboardNames,
+        tooltip: 'Call API',
+        child: const Icon(Icons.add_a_photo),
+      ),
     );
   }
+}
+
+ListView getTextListWidget(List<String>? entries) {
+  if (entries == null || entries.isEmpty) {
+    return ListView();
+  }
+
+  return ListView.builder(
+    padding: const EdgeInsets.all(16.0),
+    itemCount: (entries.length * 2) - 1,
+    itemBuilder: (context, i) {
+      if (i.isOdd) return const Divider();
+
+      final index = i ~/ 2;
+      return ListTile(
+        title: Text(
+          entries[index],
+        ),
+      );
+    },
+  );
 }
